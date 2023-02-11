09c796a72ef4491ec3af14f74e89e10cc0ebcbe8
Cho Seong week <tjddnr0217@gmail.com>
2023-02-11 21:17:01 +0900

    Feat : 추가 라우터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Reset } from "styled-reset";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Alarm from "./pages/Alarm";
+import MyPage from "./pages/MyPage";
 
 function App() {
-  return <div className="App">메인 페이지</div>;
+  return (
+    <div>
+      <Reset />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/alarm" element={<Alarm />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
