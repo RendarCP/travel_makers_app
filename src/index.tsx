c551640542c02569d6c01940b3d7ab03984f238e
Cho Seong week <tjddnr0217@gmail.com>
2023-02-13 23:29:26 +0900

    chore : 추가 slick 컴포넌트 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./style/custom.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
