3c9ee5f1f33c4bd0c0893f32230088abe2c3f2ba
Cho Seong week <tjddnr0217@gmail.com>
2023-02-15 20:27:57 +0900

    Feat : 수정 얼리버드 타입 지정

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,4 +13,32 @@ interface BannerType {
     view?: string;
 }
 
-export { type BannerType }
+interface EarlyBirdType {
+  id: number;
+  title: string;
+  hotel_id?: number;
+  explanation?: string;
+  event?: string;
+  url: string;
+  custom: {
+    button: {
+      name: string;
+    },
+    typeform: {
+      id: string;
+    }
+  };
+  layout: {
+    background: {
+      color: string;
+    }
+  };
+  time_limit: {
+    start_dt?: Date;
+    end_dt?: Date;
+  };
+  image: string;
+  image_mobile?: string;
+}
+
+export { type BannerType, type EarlyBirdType }
