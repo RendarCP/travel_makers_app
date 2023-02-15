ad5c263c1dff2db623618f197c0b1f8ec06b9559
Cho Seong week <tjddnr0217@gmail.com>
2023-02-16 01:09:46 +0900

    Feat : 추가 hotel type 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -41,4 +41,40 @@ interface EarlyBirdType {
   image_mobile?: string;
 }
 
-export { type BannerType, type EarlyBirdType }
+interface HotelType {
+  type: string;
+  hotel_id: number;
+  hotel_price_scheme: string;
+  star: string;
+  first_image_position_y: string;
+  image: string;
+  name: string;
+  description: string;
+  subway_station: string;
+  tags: Array<string>;
+  events: Array<string>;
+  price: {
+    is_price: boolean;
+    is_coupon: boolean;
+    price: {
+      max_night: number;
+      price: number;
+      discount: number;
+      sale_price: number;
+      min_night: number;
+      min_night_price: number;
+      min_night_discount: number;
+      min_night_sale_price: number;
+    }[];
+  };
+  coupons ?: string;
+  timesale: {
+    type: string;
+    format: string;
+    content?: string;
+    remaining: string;
+    end_at: string;
+  }[]
+}
+
+export { type BannerType, type EarlyBirdType, type HotelType }
