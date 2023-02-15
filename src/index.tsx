8bd837c2fccd83d8c5df87072ba5b45ecd51ae32
Cho Seong week <tjddnr0217@gmail.com>
2023-02-15 20:29:32 +0900

    Fix: 수정 index 파일 css 제거

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./style/custom.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
